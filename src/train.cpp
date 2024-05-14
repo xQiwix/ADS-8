5dc896bbfd20a08205963a611f5e55583af85356
xQiwix <159818062+xQiwix@users.noreply.github.com>
2024-05-14 19:38:10 +0300

    b176.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,60 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Train() {
+  first = nullptr;
+  cnt0 = 0;
+}
+
+Train::~Train() {
+  Cage* curnt = first;
+  while (curnt->next != first) {
+    Cage* temp = curnt;
+    curnt = curnt->next;
+    delete temp;
+  }
+}
+
+void Train::addCage(bool light) {
+  Cage* cage = new Cage;
+  cage->light = light;
+  cage->next = nullptr;
+  if (first == nullptr) {
+    cage->prev = nullptr;
+    first = cage;
+    first->next = first;
+  } else {
+    Cage* curnt = first;
+    while (curnt->next != first) {
+      curnt = curnt->next;
+    }
+    curnt->next = cage;
+    cage->prev = curnt;
+    cage->next = first;
+    first->prev = cage;
+  }
+}
+
+int Train::getLength() {
+  int len = 0;
+  Cage* curnt = first;
+  curnt->light = true;
+  while (true) {
+    curnt = curnt->next;
+    cnt0++;
+    len++;
+    if (curnt->light == true) {
+      curnt->light = false;
+      int maxlen = len;
+      len = 0;
+      for (int i = 0; i < maxlen; i++) {
+        curnt = curnt->prev;
+        cnt0++;
+      }
+      if (curnt->light == false)
+        return maxlen;
+    }
+  }
+}
+int Train::getOpCount() {
+  return countOp;
+}
